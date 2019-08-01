4208a3b3656e23072dfcce983c0552586f819d8e
郑泽帆 <zhengzefan@dianmi365.com>
2019-08-02 01:32:02 +0800

    feat(src, example): parse headers and transform response data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildUrl } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
     processConfig(config)
-    return xhr(config)
+    return xhr(config).then(res => {
+        return transfromResponseData(res)
+    })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -27,6 +29,11 @@ function transformRequestData(config: AxiosRequestConfig): any {
 function transformHeaders(config: AxiosRequestConfig): any {
     const { headers = {}, data } = config
     return processHeaders(headers, data)
+}
+
+function transfromResponseData(response: AxiosResponse): AxiosResponse {
+    response.data = transformResponse(response.data)
+    return response
 }
 
 export default axios
