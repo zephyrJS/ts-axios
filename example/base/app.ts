71e31dbdcf64b09e17cde680412dd1a4d40c9ac5
郑泽帆 <zhengzefan@dianmi365.com>
2019-08-04 00:41:59 +0800

    test(example): git comit test

--- example/base/app.ts
+++ example/base/app.ts
@@ -115,37 +115,37 @@ import axios from "../../src";
 //     console.log(res)
 // })
 
-axios({
-    url: '/error/get1'
-}).then(res => {
-    console.log(res)
-}).catch(e => {
-    console.log(e)
-})
+// axios({
+//     url: '/error/get1'
+// }).then(res => {
+//     console.log(res)
+// }).catch(e => {
+//     console.log(e)
+// })
 
-axios({
-    url: '/error/get'
-}).then(res => {
-    console.log(res)
-}).catch(e => {
-    console.log(e)
-})
+// axios({
+//     url: '/error/get'
+// }).then(res => {
+//     console.log(res)
+// }).catch(e => {
+//     console.log(e)
+// })
 
-setTimeout(()=> {
-    axios({
-        url: '/error/get'
-    }).then(res => {
-        console.log(res)
-    }).catch(e => {
-        console.log(e)
-    })
-}, 5000)
+// setTimeout(()=> {
+//     axios({
+//         url: '/error/get'
+//     }).then(res => {
+//         console.log(res)
+//     }).catch(e => {
+//         console.log(e)
+//     })
+// }, 5000)
 
-axios({
-    url: '/error/timeout',
-    timeout: 2000
-}).then(res => {
-    console.log(res)
-}).catch(e => {
-    console.log(e)
-})
+// axios({
+//     url: '/error/timeout',
+//     timeout: 2000
+// }).then(res => {
+//     console.log(res)
+// }).catch(e => {
+//     console.log(e)
+// })
