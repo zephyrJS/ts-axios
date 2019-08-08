35fe0181d15b175061e7158ec3cf41346c3708d3
ZephyrJS <zhengzefanyy@gmail.com>
2019-08-08 23:16:45 +0800

    feat(src): default config1

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -19,9 +19,11 @@ interface PromiseChain<T> {
     rejected?: RejectedFn
 }
 export class Axios {
+    defaults: AxiosRequestConfig
     interceptors: Interceptors
 
-    constructor() {
+    constructor(initConfig: AxiosRequestConfig) {
+        this.defaults = initConfig
         this.interceptors = {
             request: new InterceptorManager<AxiosRequestConfig>(),
             response: new InterceptorManager<AxiosResponse>()
